323bbe39b6fc4701929b03467468bf454ec0580d
Stephen <stephen@remine.com>
2019-10-01 17:10:06 -0500

    removing stylesheet

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './app';
-import configureStore from './store/store'
-import './index.css'
+import configureStore from './store/store';
 
 const store = configureStore();
 
-store.subscribe(() => console.log("todos updated", store.getState()));
+store.subscribe(() => console.log('todos updated', store.getState()));
 
 ReactDOM.render(
   <Provider store={store}>
